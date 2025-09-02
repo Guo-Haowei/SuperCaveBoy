48e53eab04ab5e5c34d82bef1b1df33e0f29b16d
Haowei Guo <Guo-Haowei@users.noreply.github.com>
2025-09-02 23:26:57 +0900

    minor

--- src/objects/camera.ts
+++ src/objects/camera.ts
@@ -26,26 +26,27 @@ export class Camera {
         this.offsetY = y;
     }
 
-    tick() {
-        this.followTarget();
+    tick(dt: number) {
+        this.followTarget(dt);
     }
 
     setTarget(obj) {
         this.target = obj;
     }
 
-    private followTarget() {
+    private followTarget(dt: number) {
         if (!this.target) {
             return;
         }
 
         const halfWidth = 0.5 * WIDTH;
         const halfHeight = 0.5 * HEIGHT;
+        const speed = 1 / dt;
 
-        const objx = this.target.x;
-        const objy = this.target.y;
-        this.offsetX += (objx - this.offsetX) / 20.0;
-        this.offsetY += (objy - this.offsetY) / 20.0;
+        const targetX = this.target.x;
+        const targetY = this.target.y;
+        this.offsetX += (targetX - this.offsetX) * speed;
+        this.offsetY += (targetY - this.offsetY) * speed;
 
         this.offsetX = clamp(this.offsetX, halfWidth, WWIDTH - halfWidth);
         this.offsetY = clamp(this.offsetY, halfHeight + YOFFSET, WHEIGHT - halfHeight);
